2f4d18c73b9606dfb678b94f18cb643376b87377
Rebecca Turner <me@re-becca.org>
2017-09-27 14:07:28 -0700

    set: Explicitly support setting passwords

--- cmds/set.js
+++ cmds/set.js
@@ -3,6 +3,8 @@ module.exports = set
 const npmrc = require('./util/npmrc.js')
 const profile = require('../lib')
 const validateCIDR = require('./util/validate-cidr.js')
+const read = require('./util/read.js')
+const retryWithOTP = require('./util/retry-with-otp')
 
 const blacklist = [ 'email_verified', 'tfa' ]
 
@@ -17,10 +19,18 @@ async function set (argv) {
     const info = {}
     if (argv.property === 'cidr_whitelist') {
       info.cidr_whitelist = validateCIDR.list(argv.value)
+    } else if (argv.property === 'password') {
+      const oldpassword = await read.password('Current password: ')
+      const newpassword = await read.password('New password:     ')
+      info.password = {'old': oldpassword, 'new': newpassword}
     } else {
       info[argv.property] = argv.value
     }
-    const result = await profile.set(info, {registry: argv.registry, auth: {token, otp: argv.otp}})
+    const result = await retryWithOTP({
+      otp: argv.otp,
+      get: () => read.otp('Authenticator provided OTP:'),
+      fn: otp => profile.set(info, {registry: argv.registry, auth: {token, otp}})
+    })
     console.log('Set', argv.property, 'to', result[argv.property])
   } catch (ex) {
     if (ex.code === 401) {
