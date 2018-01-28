d3928dbf519cc1d959c940bf4b3fb1f47d3c6300
isaacs <i@izs.me>
2018-01-27 23:30:58 -0800

    Tests for erroneous logins

--- tap-snapshots/lib-test-login.js-TAP.test.js
+++ tap-snapshots/lib-test-login.js-TAP.test.js
@@ -6,5 +6,26 @@
  */
 'use strict'
 exports[`lib/test/login.js TAP cleanup > got expected requests 1`] = `
-[]
+[ 'GET /weblogin/-/v1/login/blerg',
+  'GET /weblogin/-/v1/login/blerg',
+  'GET /weblogin/-/v1/login/blerg',
+  'GET /weblogin/-/v1/login/blerg',
+  'GET /weblogin/-/v1/login/blerg',
+  'POST /501/-/v1/login',
+  'POST /501/-/v1/login',
+  'POST /501/-/v1/login',
+  'POST /501/-/v1/login',
+  'POST /couchdb/-/v1/login',
+  'POST /couchdb/-/v1/login',
+  'POST /weblogin/-/v1/login',
+  'POST /weblogin/-/v1/login',
+  'POST /weblogin/-/v1/login',
+  'POST /weblogin/-/v1/login',
+  'POST /weblogin/-/v1/login',
+  'PUT /501/-/user/org.couchdb.user:user',
+  'PUT /501/-/user/org.couchdb.user:user',
+  'PUT /couchdb/-/user/org.couchdb.user:user',
+  'PUT /couchdb/-/user/org.couchdb.user:user',
+  'PUT /couchdb/-/user/org.couchdb.user:user',
+  'PUT /couchdb/-/user/org.couchdb.user:user' ]
 `
